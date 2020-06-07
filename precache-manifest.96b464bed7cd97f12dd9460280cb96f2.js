b030315322b281442d9830f780ea6fc5f7d405c0
David Wu <dwu97@yahoo.com>
2020-06-06 23:58:38 -0400

    Updates

---
+++ precache-manifest.96b464bed7cd97f12dd9460280cb96f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0959539de77fdfc6e09e9f37f74a517a",
+    "url": "/scalleycats/index.html"
+  },
+  {
+    "revision": "076abd388fe42eae340c",
+    "url": "/scalleycats/static/css/main.bfffda0a.chunk.css"
+  },
+  {
+    "revision": "e54cf215aa6785fc6271",
+    "url": "/scalleycats/static/js/2.861cb9e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/scalleycats/static/js/2.861cb9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076abd388fe42eae340c",
+    "url": "/scalleycats/static/js/main.c2104845.chunk.js"
+  },
+  {
+    "revision": "a461855f4a4c701d815f",
+    "url": "/scalleycats/static/js/runtime-main.d950289d.js"
+  },
+  {
+    "revision": "9d04584a79a54c8fb8ac7638abf61b1e",
+    "url": "/scalleycats/static/media/up-arrow.9d04584a.svg"
+  }
+]);
